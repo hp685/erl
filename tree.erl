1ce0b849d31397831b10be374c761d43490a9ee2
Harsh Patel <harsh@nyu.edu>
2013-10-25 14:06:17 -0700

    tree lookup

--- tree.erl
+++ tree.erl
@@ -3,12 +3,24 @@
 
 empty() ->
     {node, 'nil'}.
-
+%Nil node
 insert(Key, Val, {node, 'nil'}) ->
     {node, {Key, Val, {node, 'nil'}, {node, 'nil'}}};
-insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key -> {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
-insert(NewKey, NewVal, {node, {Key, Val, Smaller Larger}}) when NewKey > Key -> {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
-insert(Key, Val, {node, {Key, _ , Smaller, Larger) -> 
+%Recurse into the smaller branch
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key -> 
+    {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
+%Recurse into larger
+insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey > Key -> 
+    {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
+%Equality
+insert(Key, Val, {node, {Key, _ , Smaller, Larger}}) -> 
 			 {node, {Key, Val, Smaller, Larger}}.
 	 
-	
+lookup(_,{node,'nil'})->	
+    undefined;
+lookup(Key, {node, {Key, Val,_,_}}) ->	
+    {ok,Val};
+lookup(Key, {node, {NodeKey, _, Smaller, _}}) when Key < NodeKey -> 
+    lookup(Key, Smaller);
+lookup(Key, {node,{_,_,_,Larger}}) ->
+    lookup(Key, Larger).
