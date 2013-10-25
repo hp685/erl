b339de47f362da0825fcc31e3dfba85e68215ad1
Harsh Patel <harsh@nyu.edu>
2013-10-25 13:15:35 -0700

    tree

--- tree.erl
+++ tree.erl
@@ -9,6 +9,6 @@ insert(Key, Val, {node, 'nil'}) ->
 insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}}) when NewKey < Key -> {node, {Key, Val, insert(NewKey, NewVal, Smaller), Larger}};
 insert(NewKey, NewVal, {node, {Key, Val, Smaller Larger}}) when NewKey > Key -> {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
 insert(Key, Val, {node, {Key, _ , Smaller, Larger) -> 
-
+			 {node, {Key, Val, Smaller, Larger}}.
 	 
 	
